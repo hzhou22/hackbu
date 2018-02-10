fc415de7cf33a8dbbee3f8be1bf6279c083ee271
ajstensland <35818081+ajstensland@users.noreply.github.com>
2018-02-10 17:45:22 -0500

    Updating the datainterpreter

--- datainterpreter.py
+++ datainterpreter.py
@@ -1,27 +1,38 @@
 class KeywordCollection:
-    def __init__(self, dict):
-        self.dictionary = dict
+    def __init__(self, dictionary):
+        self.dictionary = dictionary
         self.keywords = []
-        self.eatDictionary(dict)
+        self.eatDictionary(dictionary)
 
-    def eatDictionary(self, dict):
-        for date in dict:
-            value_to_assign = dict[date]["daily_change"]
-            for i in range(len(dict[date]["articles"])):
-                title_words = dict[date]["articles"][i]["title"].split()
+    def eatDictionary(self, dictionary):
+        for date in dictionary:
+            value_to_assign = dictionary[date]["daily_change"]
+            for i in range(len(dictionary[date]["articles"])):
+                title_words = dictionary[date]["articles"][i]["title"].split()
                 description_words = []
-                if dict[date]["articles"][i]["description"] is not None:
-                    description_words = dict[date]["articles"][i]["title"].split()
+                if dictionary[date]["articles"][i]["description"] is not None:
+                    description_words = dictionary[date]["articles"][i]["title"].split()
                 all_words = title_words + description_words
-#                filtered_words =                                                                                        # Add a filter function.
+                filtered_words =                                                                                        # Add a filter function.
                 for word in all_words:
                     if self.isDuplicate(word):
-                        self.keywords[self.findIndex(word)].addAppearance(value)
-                    self.keywords.append(Keyword(word, value_to_assign))
+                        self.keywords[self.findIndex(word)].addAppearance(value_to_assign)
+                    else:
+                        self.keywords.append(Keyword(word, value_to_assign))
+        for keyword in self.keywords:
+            keyword.calculateWeight()
+
+    def filterGarbageWords(self, words):
+        garbage_words = ["for", "and", "nor", "but", "or", "yet", "so",
+                         "he", "she", "it", "them", "they"]
+        for word in words:
+            for garbage_word in garbage_words:
+                if word == garbage_word:
+                    words.remove()
 
     def isDuplicate(self, word):
         flag = False
-        for keyword in self.words:
+        for keyword in self.keywords:
             if word == keyword.getWord():
                 flag = True
         return flag
@@ -33,7 +44,6 @@ class KeywordCollection:
 
 
 class Keyword:
-
     def __init__(self, word, appearance):
         self.word = word
         self.weight = 0
@@ -46,4 +56,4 @@ class Keyword:
         self.appearances.append(value)
 
     def calculateWeight(self):
-        total = sum(self.appearances)
+        self.weight = sum(self.appearances)/len(self.appearances)
