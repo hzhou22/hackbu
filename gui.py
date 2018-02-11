899493515f6c7284c919ceba3ccda019dfaebdef
ajstensland <35818081+ajstensland@users.noreply.github.com>
2018-02-11 09:56:31 -0500

    Lots of UI updates

--- gui.py
+++ gui.py
@@ -2,16 +2,23 @@ import kivy
 kivy.require("1.10.0")
 from kivy.app import App
 from kivy.uix.widget import Widget
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
 
+class predictButton(Button):
+    def __init__(self):
+        super(predictButton, self).__init__()
 
-class ButtonPanel(Widget):
-    pass
+        self.bind(on_press=self.clicked)
+
+    def clicked(self, obj):
+        print("Hello potato")
 
 
 class StockPredictorApp(App):
 
     def build(self):
-        return ButtonPanel()
+        return BoxLayout()
 
 
 stock_predictor = StockPredictorApp()
--- main.py
+++ main.py
@@ -6,9 +6,6 @@ def main():
     stockInput = input("Enter a stock ticker: ")
     stockInfoDict = data.getInfoDict(stockInput)
     dataCollection = datainterpretation.KeywordCollection(stockInfoDict)
-<<<<<<< HEAD
-=======
     dayDictionary = dataCollection.getDayDictionary()
->>>>>>> 02d8dea054babf4dd1456fe63a05e88e141e1619
 
 main()
